ec89bab586b4b489a33afb517cc52ad5500b2460
Felix <felix@pop-os.localdomain>
2021-02-04 15:15:17 +0100

    import typed.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Can also be included with a regular script tag
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['Félix', 'Félix', 'an intern', 'a developer'],
+  typeSpeed: 100,
+  loop: 40
+};
+
+var typed = new Typed('.element', options);
